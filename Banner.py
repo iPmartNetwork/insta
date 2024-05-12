b3e0c912c76121981a725d0a232903610694db4c
ali (zeus) <131793522+iPmartNetwork@users.noreply.github.com>
2024-05-13 01:40:24 +0330

    Create Banner.py

---
+++ Banner.py
@@ -0,0 +1,31 @@
+'''
+Don't forget to give credits : D
+Author : https://github.com/ipmartnetwork
+Original Repositary : https://github.com/ipmartnetwork/Insta
+'''
+
+import time
+
+def show_banner():
+    print('\n')
+    print('''
+          _          _             _          _            _                     _           _            _          _            _         _       
+____________________________________________________________________________________
+        ____                             _     _                                     
+    ,   /    )                           /|   /                                  /   
+-------/____/---_--_----__---)__--_/_---/-| -/-----__--_/_-----------__---)__---/-__-
+  /   /        / /  ) /   ) /   ) /    /  | /    /___) /   | /| /  /   ) /   ) /(    
+_/___/________/_/__/_(___(_/_____(_ __/___|/____(___ _(_ __|/_|/__(___/_/_____/___\__
+          
+          Telegramm - https://t.me/iPmart_Network
+          Github - https://github.com/ipmartnetwork/insta                                                                             
+          Developer - https://t.me/ipmart_network
+          Donate to support this project - TRX (trc20) ~ TJbTYV1fFo2485sYMyajxGPLFzxmNmPrNA
+
+          Disclaimer - This hacking tool is intended for educational and research purposes only. 
+          Misuse of this tool for unauthorized access to computer systems or networks is strictly prohibited and may result in legal consequences. 
+          The author of this tool takes no responsibility for any damage or harm caused by its usage. 
+          It is the user's responsibility to ensure compliance with all applicable laws and regulations while using this tool. 
+          By downloading, installing, or using this tool, you agree to use it responsibly and lawfully.
+          ''')
+    time.sleep(2)
